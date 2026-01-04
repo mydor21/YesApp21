be7b318fcdbcb42bcc6c4a81e98de8c7e350a625
mydor21 <35958081+mydor21@users.noreply.github.com>
2026-01-04 20:41:20 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -6,5 +6,9 @@ import App from './App';
 const rootElement = document.getElementById('root');
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
-  root.render(<React.StrictMode><App /></React.StrictMode>);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
